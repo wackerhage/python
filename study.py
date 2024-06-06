83c838b7489a86eb0bc34403b1004a44d0471f81
Dominique Wackerhage <110652367+wackerhage@users.noreply.github.com>
2024-06-06 10:38:49 -0300

    Add files via upload

---
+++ study.py
@@ -0,0 +1,97 @@
+'''from time import sleep
+
+for i in range(20, -1, -1):
+    print(i)
+    sleep(1)
+
+print("A contagem acabou.")'''
+
+'''count = 0
+sum = 0
+
+for i in range(1, 101, 2):
+    print(i)
+    count += 1
+    sum += i
+
+print(f"The count is {count}, and the sum is {sum}.")'''
+
+'''num = int(input("Type a number: "))
+
+num_divided = 0
+prime = bool
+
+for i in range(1, num + 1):
+    if num % i == 0:
+        num_divided += 1
+
+if num_divided <= 2:
+    prime = True
+    print("It's a prime number!")
+else:
+    prime = False
+    print("It's not a prime number.")
+    print(f"This number is divided by {num_divided} times.")'''
+
+# Verificar se a frase ou palavra é um palindromo
+# (Se lê igual da direita ou da esquerda)
+
+'''phrase = str(input("Digite uma frase: ")).strip().upper()
+
+words = phrase.split()
+characters = ''.join(words)
+inverted_phrase = ''
+
+for i in range(len(characters)-1, -1, -1):
+    inverted_phrase += characters[i]
+
+print(characters, inverted_phrase)
+
+if inverted_phrase == characters:
+    print("É um palindromo!")
+else:
+    print("Nao é um palindromo.")
+'''
+
+'''nome = str(input("Digite um nome: "))
+
+print(f"Seja bem-vindo(a), {nome}!")'''
+
+'''print(int(input("Digite um número: "))**2)'''
+
+nomes = ["Ana", "Carlos", "Daiane", "Fernando", "Maria"]
+
+nomes[2] = "Jamile"
+
+nomes.append("Paulo")
+
+nomes.insert(2, "Eliane")
+
+nomes.pop(1)
+
+print(nomes[1], nomes[2], nomes[3])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
