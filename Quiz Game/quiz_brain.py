64ee97e6e36bfa491918519d2a5fced997602f37
Dominique Wackerhage <110652367+wackerhage@users.noreply.github.com>
2025-02-17 20:23:09 -0300

    Update quiz_brain.py

--- Quiz Game/quiz_brain.py
+++ Quiz Game/quiz_brain.py
@@ -1,26 +1,26 @@
+import html
+
 class QuizBrain:
 
     def __init__(self, q_list):
         self.question_number = 0
-        self.question_list = q_list
         self.score = 0
+        self.question_list = q_list
+        self.current_question = None
 
     def still_has_questions(self):
-        return len(self.question_list) != self.question_number
-
+        return self.question_number < len(self.question_list)
 
     def next_question(self):
-        current_question = self.question_list[self.question_number]
+        self.current_question = self.question_list[self.question_number]
         self.question_number += 1
-        user_answer = input(f"\nQ.{self.question_number}: {current_question.text} (True/False): ")
-        self.check_answer(user_answer, current_question.answer)
+        q_text = html.unescape(self.current_question.text)
+        return f"Q.{self.question_number}: {q_text} (True/False): "
 
-    def check_answer(self, user_answer, correct_answer):
+    def check_answer(self, user_answer):
+        correct_answer = self.current_question.answer
         if user_answer.lower() == correct_answer.lower():
-            print("You got it right!")
             self.score += 1
+            return True
         else:
-            print("Wrong answer!")
-        print(f"The answer was: {correct_answer}.")
-        print(f"Your current score is: {self.score}/{self.question_number}.")
-
+            return False
